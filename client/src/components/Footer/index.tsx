dfdd2e83764f177edc01aedf68c2905a50ca20ca
manon labat <manonlabat10@gmail.com>
2025-02-13 17:36:32 +0100

    newfiles

--- client/src/components/Footer/index.tsx
+++ client/src/components/Footer/index.tsx
@@ -2,7 +2,9 @@ import "./style.css";
 export default function Footer() {
   return (
     <footer className="end">
-      <a href="#" className="titleFooter">Retour en haut de page</a>
+      <a href="#top" className="titleFooter">
+        Retour en haut de page
+      </a>
     </footer>
   );
 }
--- client/src/components/NavBar/index.tsx
+++ client/src/components/NavBar/index.tsx
@@ -4,11 +4,13 @@ import { Link } from "react-router-dom";
 export default function UserNavBar() {
   return (
     <header className="navBar">
-     <Link to="/"> <img
-        src="/public/logo-pdf.pdf_-_Copie-removebg-preview.png"
-        alt="logo"
-        className="logo"
-      />
+      <Link to="/">
+        {" "}
+        <img
+          src="/public/logo-pdf.pdf_-_Copie-removebg-preview.png"
+          alt="logo"
+          className="logo"
+        />
       </Link>
       <h1 className="titleNav">Urgence plomberie</h1>
       <Link to="/contact" className="buttonNav">
